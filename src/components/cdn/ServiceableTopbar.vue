654887f1ab292ed265d309e4379577bb7cbe11d1
Ammerlaan,Julius J.J <j.ammerlaan@student.fontys.nl>
2019-04-08 13:16:54 +0200

    Table topbar

---
+++ src/components/cdn/ServiceableTopbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-form v-model="valid" class="ServiceableTopbar">
+    <v-container>
+      <v-layout>
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            :counter="10"
+            label="name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="type"
+            :rules="nameRules"
+            :counter="10"
+            label="type"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="FILEUPLOAD"
+            label="FILE"
+            required
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      name: '',
+      type: '',
+      nameRules: [
+        v => !!v || 'this is required'
+      ],
+      FILE: ''
+    })
+  }
+</script>
+
+<style>
+.ServiceableTopbar{
+  background: white;
+}
+</style>
--- src/views/cdn/Cdn.vue
+++ src/views/cdn/Cdn.vue
@@ -1,19 +1,33 @@
 <template>
-  <ServiceableTable/>
+  <v-layout align-center justify-center class="background-content-container" row wrap fill-height>
+    <h1>Serviceable Content</h1>
+    <div class="ServiceableRow">
+      <ServiceableTopbar class="ServiceableRow"/>
+      <ServiceableTable class="ServiceableRow"/>
+    </div>
+  </v-layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import ServiceableTable from '@/components/cdn/ServiceableTable';
+import ServiceableTable from "@/components/cdn/ServiceableTable";
+import ServiceableTopbar from "@/components/cdn/ServiceableTopbar";
 
 export default {
   name: "cdn",
   components: {
-    ServiceableTable
+    ServiceableTable,
+    ServiceableTopbar
   },
   data() {
-    return{
-    }
-  },
+    return {};
+  }
 };
 </script>
+
+<style>
+.ServiceableRow {
+  width: 100%;
+  margin-bottom: 4px;
+}
+</style>
