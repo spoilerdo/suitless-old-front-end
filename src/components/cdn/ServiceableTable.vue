6815896f86e0323b5752fb65ebf474e4cb074178
Ammerlaan,Julius J.J <j.ammerlaan@student.fontys.nl>
2019-04-09 14:48:52 +0200

    @click functions

--- src/components/cdn/ServiceableTable.vue
+++ src/components/cdn/ServiceableTable.vue
@@ -4,9 +4,9 @@
       <td>{{ props.item.name }}</td>
       <td class="text-xs-left">{{ props.item.size }}</td>
       <td class="text-xs-left">{{ props.item.type }}</td>
-      <td class="text-xs-left">
-        <v-btn color="info" small>View</v-btn>
-        <v-btn color="danger" small>Delete</v-btn>
+      <td class="text-xs-left actionsSection">
+        <v-btn color="info" small @click="viewImage(props.item.baseURL)">View</v-btn>
+        <v-btn color="danger" small @click="deleteImage(props.item.baseURL)">Delete</v-btn>
       </td>
     </template>
   </v-data-table>
@@ -26,10 +26,22 @@ export default {
         {
           name: "ehvLINC Logo",
           size: "0.6",
-          type: "image/png"
+          type: "image/png",
+          baseURL: "nope",
         }
       ]
     };
+  },
+  methods: {
+    uploadImage() {},
+    viewImage(baseURL) { console.log("SHOW", baseURL); },
+    deleteImage(baseURL) { console.log("DELETE", baseURL); }
   }
 };
 </script>
+
+<style>
+.actionsSection {
+  max-width: 80px;
+}
+</style>
