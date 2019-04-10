bd7fb639cb1a903d44fc0ab2ec3e8ae8eceaabe5
Ammerlaan,Julius J.J <j.ammerlaan@student.fontys.nl>
2019-04-10 13:59:13 +0200

    Simple CDN viewer

--- src/components/cdn/ServiceableTable.vue
+++ src/components/cdn/ServiceableTable.vue
@@ -1,19 +1,33 @@
 <template>
-  <v-data-table :headers="headers" :items="serviceables" :rows-per-page-items=[4] class="elevation-1">
-    <template v-slot:items="props">
-      <td>{{ props.item.name }}</td>
-      <td>{{ props.item.size }}</td>
-      <td>{{ props.item.type }}</td>
-      <td class="actionsSection">
-        <v-btn color="info" small @click="viewContent(props.item.baseURL)">View</v-btn>
-        <v-btn color="danger" small @click="deleteContent(props.item.baseURL)">Delete</v-btn>
-      </td>
-    </template>
-  </v-data-table>
+  <v-layout column style="height: 59vh">
+    <v-flex md6 style="overflow: auto">
+      <v-data-table :headers="headers" :items="serviceables" hide-actions class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.name }}</td>
+          <td>{{ props.item.size }}</td>
+          <td>{{ props.item.type }}</td>
+          <td class="actionsSection">
+
+            <v-dialog v-model="dialog">
+              <template v-slot:activator="{ on }">
+                <v-btn color="blue lighten-2" small v-on="on">view</v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>{{ props.item.name }}</v-card-title>
+                <iframe class="cdnContent" :src="props.item.baseURL"></iframe>
+              </v-card>
+            </v-dialog>
+
+            <v-btn color="danger" small @click="deleteContent(props.item.baseURL)">Delete</v-btn>
+          </td>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import * as api from "@/api/api"
+import * as api from "@/api/api";
 
 export default {
   data() {
@@ -24,8 +38,7 @@ export default {
         { text: "Type", value: "type" },
         { text: "Actions", sortable: false }
       ],
-      serviceables: [
-      ]
+      serviceables: []
     };
   },
   methods: {
@@ -41,17 +54,17 @@ export default {
       //TODO: DE URI uit env.
       let apiURI = "http://localhost:3305/";
 
-      api.apiCall("GET", apiURI + "meta/all").then((data) => {
+      api.apiCall("GET", apiURI + "meta/all").then(data => {
         console.log(data);
         data.metadataList.forEach(serviceable => {
           this.serviceables.push({
             name: serviceable.tag,
-            size: (serviceable.size/1000).toFixed(2), //Byte to MB
+            size: (serviceable.size / 1000).toFixed(2), //Byte to MB
             type: serviceable.type,
             baseURL: apiURI + serviceable.tag
-          })
+          });
         });
-      })
+      });
     }
   },
   mounted() {
@@ -63,5 +76,10 @@ export default {
 <style>
 .actionsSection {
   max-width: 100px;
+}
+
+.cdnContent{
+  width: 100%;
+  height: 100%;
 }
 </style>
