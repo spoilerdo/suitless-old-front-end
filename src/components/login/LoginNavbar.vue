c242a005d3aefc1442a507c253658f5ae9f58ac1
Ammerlaan,Julius J.J <j.ammerlaan@student.fontys.nl>
2019-04-17 11:42:23 +0200

    PDF and Login from landing page

---
+++ src/components/login/LoginNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-flex text-xs-right justify-center class="margin">
+      <v-btn to="/login" small class="primary">Login</v-btn>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  })
+};
+</script>
+
+<style>
+.margin{
+    padding-right: 20px;
+    padding-top: 10px;
+    margin-bottom: -10px;
+}
+</style>
+
+
