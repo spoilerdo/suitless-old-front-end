e89d705ccc4909bff2eff24055ac0ba1caf4536b
Ammerlaan,Julius J.J <j.ammerlaan@student.fontys.nl>
2019-05-21 11:05:03 +0200

    CDN Unit Tests

--- tests/e2e/specs/cdn.js
+++ tests/e2e/specs/cdn.js
@@ -1,7 +1,9 @@
+
 module.exports = {
     'CDN Title check': function (browser) {
         // Browser is the browser that is being controlled
         browser
+            .useCss()
             .url('http://localhost:8080/cdn') // Navigate to the url
             .waitForElementVisible('body', 1000) // Wait until you can see the body element.
             .verify.title("ehvLINC") //verify if the title is correct
@@ -11,11 +13,12 @@ module.exports = {
     'CDN Upload Bar exists': function (browser) {
         // Browser is the browser that is being controlled
         browser
+            .useCss()
             .url('http://localhost:8080/cdn') // Navigate to the url
             .waitForElementVisible('body', 1000) // Wait until you can see the body element.
-            .assert.visible('button.v-btn.theme--light.info') //Upload button
-            .assert.visible('div.v-input.v-text-field.theme--light') //Tag input
-            .assert.visible('div.v-input.v-text-field.v-input--is-readonly.theme--light') //Select Image
+            .assert.visible('#uploadBtn') //Upload button
+            .assert.visible('#nameField') //Tag input
+            .assert.visible('#fileField') //Select Image
             .assert.containsText('b', 'Create a new serviceable') //Title of bar
             .end() // This must be called to close the browser at the end
     },
@@ -23,33 +26,38 @@ module.exports = {
     'CDN Table exists': function (browser) {
         // Browser is the browser that is being controlled
         browser
+            .useCss()
             .url('http://localhost:8080/cdn') // Navigate to the url
             .waitForElementVisible('body', 1000) // Wait until you can see the body element.
-            .assert.visible('table.v-datatable.v-table.theme--light') //Check table
+            .useXpath()
+            .assert.visible('//table') //Check table
             .end() // This must be called to close the browser at the end
     },
 
-    'CDN Upload warning': function (browser) {
+    'CDN No tag warning': function (browser) {
         // Browser is the browser that is being controlled
         browser
+            .useCss()
             .url('http://localhost:8080/cdn') // Navigate to the url
             .waitForElementVisible('body', 1000) // Wait until you can see the body element.
-            //Enter a correct tag name.
-            //Click the upload button.
-            //check for file warning.
+            .click('#uploadBtn')
+            .pause(100)
+            .useXpath()
+            .assert.containsText('//div/span', 'The tag field is required.')
             .end() // This must be called to close the browser at the end
     },
 
-    'CDN Upload success': function (browser) {
+    'CDN Short tag warning': function (browser) {
         // Browser is the browser that is being controlled
         browser
+            .useCss()
             .url('http://localhost:8080/cdn') // Navigate to the url
             .waitForElementVisible('body', 1000) // Wait until you can see the body element.
-            //Enter a correct tag name.
-            //Enter a correct debug name.
-            //Click upload button.
-            //Wait for the upload
-            //Check the feedback
+            .sendKeys('#nameField', 'sh')
+            .pause(100)
+            .click('#uploadBtn')
+            .useXpath()
+            .assert.containsText('//div/span', 'The tag field must be at least 3 characters.')
             .end() // This must be called to close the browser at the end
     }
 }
