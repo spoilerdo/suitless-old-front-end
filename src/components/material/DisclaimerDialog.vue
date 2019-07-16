b8dace00d5a59b3cb022ca027ca87a81db036ba6
Martijn <m.dormans@student.fontys.nl>
2019-07-16 16:01:11 +0200

    changed disclaimer width to 700

--- src/components/material/DisclaimerDialog.vue
+++ src/components/material/DisclaimerDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="290">
+  <v-dialog v-model="dialog" persistent max-width="700">
     <v-card>
       <v-card-title class="headline">Disclaimer</v-card-title>
       <v-card-text>The Legal Healthcheck Platform is not your online lawyer. Therefore, the service itself and the information it provides shall not be seen as legal advice. Rather, the Legal Healthcheck platform is intended as educational tool for students and start-ups to obtain basic legal knowledge. EhvLINC cannot guarantee that the information provided on this website (including the reports that it generates) is correct and fully up-to-date. Usage of the Legal Healthcheck Platform is fully at your own risk. EhvLINC explicitly rejects all liability for direct, indirect, incidental or special damages arising out or relating to the access or use of the service. This includes, but not restricted to, loss or damage caused by usage of information by this website, inaccurate results, loss of profits, business interruption, loss of use of the service, the cost of substitute services or claims by third parties for any damages to computers, software, modems, telephones or other property.</v-card-text>
